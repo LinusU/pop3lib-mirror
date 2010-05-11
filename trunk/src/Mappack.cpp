5f8b09967c4e7d8a721e589fd77d8d6f2d909fc8
ddaroo <ddaroo@636f87cb-5dee-4d52-bda0-98043e134423>
2010-05-11 11:24:57 +0000

    Preparations for mappack handling

--- trunk/src/Mappack.cpp
+++ trunk/src/Mappack.cpp
@@ -17,43 +17,11 @@ You should have received a copy of the GNU General Public License
 along with poplib. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "Ip.h"
-#include "PopGame.h"
 #include "Mappack.h"
 
 namespace poplib
 {
 
-PopGame::PopGame() : mdir("")
-{
-
-}
-
-PopGame::PopGame ( const std::string& dir ) : mdir(dir)
-{
-
-}
-
-PopGame::~PopGame()
-{
-
-}
-
-#if defined(PLATFORM_WIN32)
-bool PopGame::startMultiplayer ( const std::string& playerName, Ip localIp, Ip ExternIp )
-{
-    return false;
-}
-#endif
-
-void PopGame::loadMappack(Mappack* mappack)
-{
-    // TODO loadMappack() method in PopGame class
-}
-
-void PopGame::restoreMaps()
-{
-    // TODO restoreMaps() method in PopGame class
-}
+	
 
 } // namespace poplib
