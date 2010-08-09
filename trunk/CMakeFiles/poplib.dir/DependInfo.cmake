27723881149756c0e7aaeee9f3c6d3a9253dd263
ddaroo <ddaroo@636f87cb-5dee-4d52-bda0-98043e134423>
2010-08-09 00:20:25 +0000

    eclipse cmake files

---
+++ trunk/CMakeFiles/poplib.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/daroo/workspace/poplib@poplib@poplib/src/Ip.cpp" "C:/Users/daroo/workspace/poplib@poplib@poplib/CMakeFiles/poplib.dir/src/Ip.cpp.obj"
+  "C:/Users/daroo/workspace/poplib@poplib@poplib/src/Map.cpp" "C:/Users/daroo/workspace/poplib@poplib@poplib/CMakeFiles/poplib.dir/src/Map.cpp.obj"
+  "C:/Users/daroo/workspace/poplib@poplib@poplib/src/MapDat.cpp" "C:/Users/daroo/workspace/poplib@poplib@poplib/CMakeFiles/poplib.dir/src/MapDat.cpp.obj"
+  "C:/Users/daroo/workspace/poplib@poplib@poplib/src/MapHeader.cpp" "C:/Users/daroo/workspace/poplib@poplib@poplib/CMakeFiles/poplib.dir/src/MapHeader.cpp.obj"
+  "C:/Users/daroo/workspace/poplib@poplib@poplib/src/MapLand.cpp" "C:/Users/daroo/workspace/poplib@poplib@poplib/CMakeFiles/poplib.dir/src/MapLand.cpp.obj"
+  "C:/Users/daroo/workspace/poplib@poplib@poplib/src/MapObjDiscovery.cpp" "C:/Users/daroo/workspace/poplib@poplib@poplib/CMakeFiles/poplib.dir/src/MapObjDiscovery.cpp.obj"
+  "C:/Users/daroo/workspace/poplib@poplib@poplib/src/MapObjTrigger.cpp" "C:/Users/daroo/workspace/poplib@poplib@poplib/CMakeFiles/poplib.dir/src/MapObjTrigger.cpp.obj"
+  "C:/Users/daroo/workspace/poplib@poplib@poplib/src/Mappack.cpp" "C:/Users/daroo/workspace/poplib@poplib@poplib/CMakeFiles/poplib.dir/src/Mappack.cpp.obj"
+  "C:/Users/daroo/workspace/poplib@poplib@poplib/src/MappackMap.cpp" "C:/Users/daroo/workspace/poplib@poplib@poplib/CMakeFiles/poplib.dir/src/MappackMap.cpp.obj"
+  "C:/Users/daroo/workspace/poplib@poplib@poplib/src/PopGame.cpp" "C:/Users/daroo/workspace/poplib@poplib@poplib/CMakeFiles/poplib.dir/src/PopGame.cpp.obj"
+  "C:/Users/daroo/workspace/poplib@poplib@poplib/src/StrUtil.cpp" "C:/Users/daroo/workspace/poplib@poplib@poplib/CMakeFiles/poplib.dir/src/StrUtil.cpp.obj"
+  "C:/Users/daroo/workspace/poplib@poplib@poplib/src/String16.cpp" "C:/Users/daroo/workspace/poplib@poplib@poplib/CMakeFiles/poplib.dir/src/String16.cpp.obj"
+  "C:/Users/daroo/workspace/poplib@poplib@poplib/src/UTFConversion.cpp" "C:/Users/daroo/workspace/poplib@poplib@poplib/CMakeFiles/poplib.dir/src/UTFConversion.cpp.obj"
+  "C:/Users/daroo/workspace/poplib@poplib@poplib/src/systemUtils/Dir.cpp" "C:/Users/daroo/workspace/poplib@poplib@poplib/CMakeFiles/poplib.dir/src/systemUtils/Dir.cpp.obj"
+  "C:/Users/daroo/workspace/poplib@poplib@poplib/src/systemUtils/File.cpp" "C:/Users/daroo/workspace/poplib@poplib@poplib/CMakeFiles/poplib.dir/src/systemUtils/File.cpp.obj"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
